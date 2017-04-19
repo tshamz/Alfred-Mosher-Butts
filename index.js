06583387e479a63b3c095545a9cc774d91bc60b8
Tyler Shambora <tyler@bvaccel.com>
2017-04-19 11:43:29 -0700

    update sent url

--- index.js
+++ index.js
@@ -5,21 +5,17 @@ let request = require('request-promise');
 request('http://urbandictionary.com').then(function (html) {
   let $ = cheerio.load(html);
   let word = $('.word').first().text();
-
   return word;
-
-
 }).then(function (word) {
   request(`http://api.urbandictionary.com/v0/define?term=${word}`).then(function (result) {
+    let data = JSON.parse(result).list[0];
     request({
       method: 'POST',
       uri: 'https://hooks.slack.com/services/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',
       body: {
-        "text": JSON.parse(result).list[0].permalink
+        "text": `http://www.urbandictionary.com/define.php?term=${data.word}&defid=${data.defid}`
       },
       json: true
     });
   });
 });
-
-
