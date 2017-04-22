ee410b64a0e7611e13adcbbe88dc53bf8bb2bf0a
Tyler Shambora <tyler@bvaccel.com>
2017-04-22 16:42:50 -0700

    use promises better and adds formatting to definition

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
-let cheerio = require('cheerio');
-let request = require('request-promise');
+const Q = require('q');
+const cheerio = require('cheerio');
+const request = require('request-promise');
 
+const getWord = function () {
+  return Q.Promise(function (resolve, reject, notify) {
+    request('http://urbandictionary.com')
+    .then(function (html) {
+      let $ = cheerio.load(html);
+      let word = $('.word').first().text();
+      resolve(word);
+    })
+    .catch(function (err) {
+      reject(new Error(err));
+    })
+  });
+};
 
-request('http://urbandictionary.com').then(function (html) {
-  let $ = cheerio.load(html);
-  let word = $('.word').first().text();
-  return word;
-}).then(function (word) {
-  request(`http://api.urbandictionary.com/v0/define?term=${word}`).then(function (result) {
-    let data = JSON.parse(result).list[0];
-    let url = `http://www.urbandictionary.com/define.php?term=${data.word}&defid=${data.defid}`;
+const getDefinition = function (word) {
+  return Q.Promise(function (resolve, reject, notify) {
+    request(`http://api.urbandictionary.com/v0/define?term=${word}`)
+    .then(function (response) {
+      let data = JSON.parse(response).list[0];
+      resolve(data);
+    })
+    .catch(function (err) {
+      reject(new Error(err));
+    })
+  });
+};
+
+const postToSlack = function (data) {
+  return Q.Promise(function (resolve, reject, notify) {
     request({
       method: 'POST',
       uri: 'https://hooks.slack.com/services/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',
-    body: {
+      body: {
         "attachments": [{
           "fallback": data.definition,
           "color": "#e86222",
           "title": data.word,
           "title_link": data.permalink,
-          "text": `${data.definition} \n\n "${data.example}"`,
+          "text": `${data.definition} \n\n _${data.example}_`,
+          "mrkdwn_in": ["text"]
         }]
       },
       json: true
-    });
+    })
+    .then(function () {
+      resolve();
+    })
+    .catch(function () {
+      reject(new Error(err));
+    })
   });
-});
+};
+
+getWord().then(getDefinition).then(postToSlack).done();
